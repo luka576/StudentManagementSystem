978b63cda031b3ba67ae9c1fa52e837406847394
luka576 <luka.manvelishvili.1@iliauni.edu.ge>
2026-05-11 00:36:30 +0400

    Update Student.cs

--- StudentManagementSystem/model/Student.cs
+++ StudentManagementSystem/model/Student.cs
@@ -1,4 +1,13 @@
-public class Student : Person
+public class Student : Person, IPrintable
 {
     public List<Grade> Grades { get; set; } = new List<Grade>();
+    public void PrintDetails()
+    {
+        Console.WriteLine($"Student ID: {Id}, Name: {Name}");
+        Console.WriteLine("Grades:");
+        foreach (var grade in Grades)
+        {
+            Console.WriteLine($"Course: {grade.TargetCourse.Name}, Score: {grade.Score}");
+        }
+    }
 }
